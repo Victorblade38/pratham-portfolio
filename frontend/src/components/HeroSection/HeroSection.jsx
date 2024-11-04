9a0a51274361c3c38c3311e718f79fc2e1395d85
prathammhavale <prathammhavale38@gmail.com>
2024-11-04 18:47:27 +0530

    navbar

--- frontend/src/components/HeroSection/HeroSection.jsx
+++ frontend/src/components/HeroSection/HeroSection.jsx
@@ -13,22 +13,22 @@ const HeroSection = () => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center mt-10">
+    <div className="flex flex-col justify-center items-center mt-20">
       {/* <img src={bg} alt="Background" className="bg-img" /> */}
       <div className="flex flex-col gap-2">
         <div className="flex flex-col gap-1">
           <span className="text-[12px] ">--- hello</span>
-          <h1 className="text-2xl font-bold">I am Pratham Mhavale</h1>
+          <h1 className="text-3xl font-bold">I am Pratham Mhavale</h1>
           <p className="font-semibold">Frontend Developer</p>
         </div>
         <a href={resume} download>
           <button
             className={`resume-btn ${
               clicked ? "clicked" : ""
-            } bg-blue-500 px-2 py-1 text-white rounded-md`}
+            } bg-blue-500 px-4 py-2 text-white rounded-md`}
             onClick={handleClick}
           >
-            <span className="text-[12px]">Resume</span>
+            <span className="text-[14px]">Resume</span>
           </button>
         </a>
       </div>
