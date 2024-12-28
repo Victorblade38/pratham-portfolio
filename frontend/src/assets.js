57f348648b86b572d664fc20fc780a2a98ebad54
prathammhavale <prathammhavale38@gmail.com>
2024-12-28 08:55:25 +0530

    resume change

--- frontend/src/assets.js
+++ frontend/src/assets.js
@@ -2,7 +2,7 @@ import bg from "./assets/nikita-kachanovsky-OVbeSXRk_9E-unsplash.jpg";
 import personIll from "./assets/undraw_visionary_technology_re_jfp7.svg";
 import personIll2 from "./assets/undraw_profile_re_4a55.svg";
 import personIcon from "./assets/boyIcon.png";
-import cv from "./assets/PrathamCV.pdf";
+import cv from "./assets/Pratham_Mhavale_Frontend_Web_Developer_Resume.pdf";
 import linkedinLogo from "./assets/linkedin.png";
 import githubLogo from "./assets/github.png";
 import gmail from "./assets/gmail.png";
