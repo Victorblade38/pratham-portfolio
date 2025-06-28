cb29eddf415be0cb6f0538ed6eced5763c5f51d3
Pratham Mhavale <96863809+Victorblade38@users.noreply.github.com>
2025-06-29 04:35:01 +0530

    Update HeroSection.jsx

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -11,7 +11,18 @@ const HeroSection = () => {
           <span className="text-blue-600 ">Front-end Developer</span> & Aspiring{" "}
           <span className="text-blue-600 ">UI/UX Designer</span> .
         </h1>
-         <resumeButton/>
+         <a
+      href="https://docs.google.com/document/d/1o3OJXLiskZ5l5pprqH7vaKjSRdKgNJAm/edit?usp=sharing&ouid=113893774555491319344&rtpof=true&sd=true"
+      target="_blank"
+      className="xl:mt-2"
+    >
+      <motion.button
+        whileTap={{ y: 4, boxShadow: "none" }} // Moves down and removes shadow on click
+        className="font-inter text-sm xl:text-lg  font-medium bg-white text-black px-[80px] xl:px-[120px] py-[10px] md:py-[12px] border-[2px] border-gray-900 rounded-sm shadow-[4px_4px_0px_black] transition-all"
+      >
+        Resume
+      </motion.button>
+    </a>
       </div>
     </div>
   );
