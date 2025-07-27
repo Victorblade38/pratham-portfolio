896facf909050f9ac0245be83f07c19d1bce6db4
prathammhavale <prathammhavale38@gmail.com>
2025-07-27 21:46:37 +0530

    Update HeroSection.jsx

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -1,29 +1,37 @@
 import React from "react";
 import { motion } from "framer-motion";
+
+import Navbar from "./Navbar";
+
 import resumeButton from "./resumeButton";
 
 const HeroSection = () => {
   return (
-    <div className="xl:w-[1440px] mx-[24px] md:mx-[34px] py-24   flex flex-col items-center ">
-      <div className=" md:w-[650px] lg:w-[900px] xl:w-[1200px]  flex flex-col gap-4  justify-center md:items-center mt-40 ">
-        <h1 className="text-[34px] md:text-center lg:text-4xl xl:text-6xl rounded-md ">
-          Designing experiences and building Interfaces as a{" "}
-          <span className="text-blue-600 ">Front-end Developer</span> & Aspiring{" "}
-          <span className="text-blue-600 ">UI/UX Designer</span> .
+    <div className="mt-6 w-screen px-4 min-h-screen flex flex-col gap-20">
+      <div className="w-72">
+        <h1 className="w-full font-inclusive-sans text-4xl">
+          Designing and building experiences as a Front-end Developer &
+          Enthusiatic UI/UX Designer.
         </h1>
-         <a
-      href="https://docs.google.com/document/d/1o3OJXLiskZ5l5pprqH7vaKjSRdKgNJAm/edit?usp=sharing&ouid=113893774555491319344&rtpof=true&sd=true"
-      target="_blank"
-      className="xl:mt-2"
-    >
-      <motion.button
-        whileTap={{ y: 4, boxShadow: "none" }} // Moves down and removes shadow on click
-        className="font-inter text-sm xl:text-lg  font-medium bg-white text-black px-[80px] xl:px-[120px] py-[10px] md:py-[12px] border-[2px] border-gray-900 rounded-sm shadow-[4px_4px_0px_black] transition-all"
-      >
-        Resume
-      </motion.button>
-    </a>
+
+        <a
+          href="https://docs.google.com/document/d/1o3OJXLiskZ5l5pprqH7vaKjSRdKgNJAm/edit?usp=sharing&ouid=113893774555491319344&rtpof=true&sd=true"
+          target="_blank"
+          className="xl:mt-2"
+        >
+          <motion.button
+            whileTap={{ y: 4, boxShadow: "none" }} // Moves down and removes shadow on click
+            className="font-inter text-sm xl:text-lg  font-medium bg-white text-black px-[80px] xl:px-[120px] py-[10px] md:py-[12px] border-[2px] border-gray-900 rounded-sm shadow-[4px_4px_0px_black] transition-all"
+          >
+            Resume
+          </motion.button>
+        </a>
       </div>
+      <Navbar />
+      <Button>
+        <resumeButton />
+      </Button>
+      <h1 className="text-end font-inclusive-sans text-3xl">Pratham Mhavale</h1>
     </div>
   );
 };
